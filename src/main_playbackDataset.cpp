5469460e4200b783834100f8a53a91841c8f10ae
Spencer Carmichael <specarmi@umich.edu>
2021-06-08 11:45:27 -0400

    Updated playbackDataset and added it back to CMakeLists.

--- src/main_playbackDataset.cpp
+++ src/main_playbackDataset.cpp
@@ -48,46 +48,25 @@ int main( int argc, char** argv )
 	setlocale(LC_ALL, "");
 
 	std::string dataset = argv[1];
-
-
-
-
 	printf("Playback dataset %s!\n", dataset.c_str());
 
-	DatasetReader* reader = new DatasetReader(dataset);
-
-	Eigen::Matrix3f K_rect = reader->getUndistorter()->getK_rect();
-	Eigen::Vector2i dim_rect = reader->getUndistorter()->getOutputDims();
-
-	printf("Rectified Images: %d x %d. K:\n",dim_rect[0], dim_rect[1]);
-	std::cout << K_rect << "\n\n";
-
-	Eigen::Matrix3f K_org= reader->getUndistorter()->getK_org();
-	Eigen::Vector2i dim_org = reader->getUndistorter()->getInputDims();
-	float omega = reader->getUndistorter()->getOmega();
-
-	printf("Original Images: %d x %d. omega=%f K:\n",dim_org[0], dim_org[1], omega);
-	std::cout << K_org << "\n\n";
-
-
-	if(argc > 2)
+	char* trueBitDepthInput = argv[2];
+	int option;
+	int trueBitDepth;
+	float saturationVal;
+	if(1==sscanf(trueBitDepthInput,"trueBitDepth=%d",&option))
 	{
-		printf("Saving undistorted Dataset to here!\n");
-		for(int i=0;i<reader->getNumImages();i++)
-		{
-			ExposureImage* I = reader->getImage(i, true, false, false, false);
-			char buf[1000];
-			snprintf(buf, 1000, "%05d.jpg", i);
-			cv::imwrite(buf, cv::Mat(I->h, I->w, CV_32F, I->image));
-			delete I;
-		}
-		exit(0);
+		trueBitDepth = option;
+		saturationVal = static_cast<float>(pow(2, trueBitDepth) - 1);
+		printf("trueBitDepth set to %d!\n", trueBitDepth);
+	}
+	else
+	{
+		std::cout << "trueBitDepth not set, exiting.\n";
+		return -1;
 	}
 
-
-
-
-
+	DatasetReader* reader = new DatasetReader(dataset, trueBitDepth);
 
 	bool autoPlay = false;
 	bool rect = false;
@@ -95,13 +74,12 @@ int main( int argc, char** argv )
 	bool removeVignette = false;
 	bool killOverexposed = false;
 
-
 	for(int i=0;i<reader->getNumImages();i++)
 	{
 		while(true)
 		{
 			ExposureImage* I = reader->getImage(i, rect, removeGamma, removeVignette, killOverexposed);
-			cv::imshow("Image", cv::Mat(I->h, I->w, CV_32F, I->image) * (1/255.0f));
+			cv::imshow("Image", cv::Mat(I->h, I->w, CV_32F, I->image) * (1/saturationVal));
 			printf("Read image %d, time %.5f, exposure %.5fms. Rect (r): %d, remove gamma (g) %d, remove vignette (v): %d, kill overesposed (o)%d\n",
 					I->id, I->timestamp, I->exposure_time,
 					(int)rect, (int)removeGamma, (int)removeVignette, (int)killOverexposed);
@@ -110,8 +88,6 @@ int main( int argc, char** argv )
 			char k;
 			if(autoPlay) k = cv::waitKey(1);
 			else k = cv::waitKey(0);
-
-			if(k=='w' || k == 'W') cv::imwrite("img.png", cv::Mat(I->h, I->w, CV_32F, I->image));
 
 			delete I;
 
